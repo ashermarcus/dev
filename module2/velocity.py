343565be32f659e0524168efa46f1e3f2b1f3645
ashermarcus <143534020+ashermarcus@users.noreply.github.com>
2023-09-05 10:35:31 -0600

    vel calc

--- module2/velocity.py
+++ module2/velocity.py
@@ -5,6 +5,7 @@ my_time = input()
 # Task 2: Ask user for distance
 my_dist = input('How far did they go?: ')
 # Task 3: Calculate velocity, and display it
-
-print('Your time was', my_timer)
+my_vel = my_dist/my_time
+print('Your time was', my_time)
 print('Your distance was', my_dist)
+print('Your velocity is:', my_vel)
