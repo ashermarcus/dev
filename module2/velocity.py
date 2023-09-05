0e7572a2a16136f333be0b222b03c2a63eb70958
ashermarcus <143534020+ashermarcus@users.noreply.github.com>
2023-09-05 10:08:20 -0600

    task 1 complete

--- module2/velocity.py
+++ module2/velocity.py
@@ -1,6 +1,9 @@
 
 # Task 1: Ask user for the time
-
+print('What is the time?')
+my_time = input()
 # Task 2: Ask user for distance
 
 # Task 3: Calculate velocity, and display it
+
+print('Your time was', my_timer)
