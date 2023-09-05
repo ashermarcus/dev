906c90202c044d8c57a467a7cf8061df6faf38ec
ashermarcus <143534020+ashermarcus@users.noreply.github.com>
2023-09-05 10:41:31 -0600

    cast input as integers and floats

--- module2/velocity.py
+++ module2/velocity.py
@@ -2,8 +2,9 @@
 # Task 1: Ask user for the time
 print('What is the time?')
 my_time = input()
+my_time = int(my_time) # convert to int
 # Task 2: Ask user for distance
-my_dist = input('How far did they go?: ')
+my_dist = float(input('How far did they go?: ')) # cast directly as float
 # Task 3: Calculate velocity, and display it
 my_vel = my_dist/my_time
 print('Your time was', my_time)
